361cc1f1307eec980fb3e73795cae683e3cea9aa
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-13 18:17:14 +0000

    Product Red cycle TDD
    -Initial Unit test that creates a failing test as the product class is not created
    -Also renaming of Initial Unit test

---
+++ Cameron.Katka.UnitTests/ProductTests.cs
@@ -0,0 +1,21 @@
+namespace Cameron.Katka.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Product_Should_Have_Required_Data()
+        {
+            Product product = new Product("A", 50.00m, 3, 130.00m);
+            Assert.IsInstanceOf<string>(product.SKU);
+            Assert.IsInstanceOf<decimal>(product.UnitPrice);
+            Assert.IsInstanceOf<int>(product.DiscountUnits);
+            Assert.IsInstanceOf<decimal>(product.DiscountUnitPrice);
+
+        }
+    }
+}
