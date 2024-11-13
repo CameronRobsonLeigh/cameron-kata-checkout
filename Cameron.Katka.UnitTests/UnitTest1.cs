d586d718a488cbe6d677cf6e77f21847d3e98c7b
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-13 18:12:11 +0000

    Add NUnitProject
    Also renaming of class library to fit namespace

---
+++ Cameron.Katka.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Cameron.Katka.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
