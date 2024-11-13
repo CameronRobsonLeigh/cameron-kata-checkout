51c6c723dc0a84075e18114f5a26baf46da81575
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-13 21:40:04 +0000

    Multiple products to basket test

--- Cameron.Katka.UnitTests/CheckoutTests.cs
+++ Cameron.Katka.UnitTests/CheckoutTests.cs
@@ -16,8 +16,11 @@ namespace Cameron.Katka.UnitTests
         [Test]
         public void Product_Should_Scan()
         {
+            // Arrange
             Product product = new Product("A", 50.00m, 3, 130.00m);
+            // Act
             int productCount = _checkoutService.Scan(product);
+            // Assert
             Assert.AreEqual(1, productCount);
         }
 
@@ -25,6 +28,21 @@ namespace Cameron.Katka.UnitTests
         public void ScanProduct_Should_ThrowException_When_ProductIsNull()
         {
             Assert.Throws<ArgumentNullException>(() => _checkoutService.Scan(null));
+        }
+
+        [Test]
+        public void ScanMultipleProducts_Should_Increase_Basket_Count()
+        {
+            // Arrange
+            var product1 = new Product("A", 50.00m, 1, 40.00m);
+            var product2 = new Product("B", 30.00m, 2, 25.00m);
+
+            // Act
+            _checkoutService.Scan(product1); 
+            int countAfterFirstScan = _checkoutService.Scan(product2); 
+
+            // Assert
+            Assert.AreEqual(2, countAfterFirstScan);  // Basket should have 2 products now
         }
 
     }
