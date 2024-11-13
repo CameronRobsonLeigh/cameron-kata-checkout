55875c01ad6e86bb406e988360c8bc378986d59f
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-13 21:03:52 +0000

    Red phase for sad path scan product

--- Cameron.Katka.UnitTests/CheckoutTests.cs
+++ Cameron.Katka.UnitTests/CheckoutTests.cs
@@ -5,21 +5,27 @@ namespace Cameron.Katka.UnitTests
 {
     internal class CheckoutTests
     {
-        private CheckoutService _checkOutService;
+        private CheckoutService _checkoutService;
 
         [SetUp]
         public void Setup()
         {
-            _checkOutService = new CheckoutService();
+            _checkoutService = new CheckoutService();
         }
 
         [Test]
         public void Product_Should_Scan()
         {
             Product product = new Product("A", 50.00m, 3, 130.00m);
-            int productCount = _checkOutService.Scan(product);
+            int productCount = _checkoutService.Scan(product);
             Assert.AreEqual(1, productCount);
-
         }
+
+        [Test]
+        public void ScanProduct_Should_ThrowException_When_ProductIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _checkoutService.Scan(null));
+        }
+
     }
 }
