cb792a095b066da1c1b509f8b951045ae2fc8488
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-13 21:44:20 +0000

    green path for product with negative price

--- Cameron.Katka.ClassLibrary/Services/CheckoutService.cs
+++ Cameron.Katka.ClassLibrary/Services/CheckoutService.cs
@@ -10,6 +10,9 @@ namespace Cameron.Katka.ClassLibrary.Services
             if (product == null)
                 throw new ArgumentNullException(nameof(product));
 
+            if (product.UnitPrice < 0)
+                throw new ArgumentException("Product price cannot be negative", nameof(product.UnitPrice));
+
             _basket.Add(product);
 
             return _basket.Count;
