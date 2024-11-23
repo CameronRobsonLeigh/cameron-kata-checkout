dad96b76decd5ef922cedffe2da2cc6932617357
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-23 11:40:33 +0000

    Create PricingRule.cs

---
+++ Cameron.Katka.ClassLibrary/Models/PricingRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cameron.Katka.ClassLibrary.Models
+{
+    public class PricingRule
+    {
+        public string Sku { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        public int? DiscountUnits { get; set; }
+
+        public int? DiscountRate { get; set; }
+
+        public PricingRule(string sku, int unitPrice)
+        {
+            Sku = sku;
+            UnitPrice = unitPrice;
+        }
+    }
+}
