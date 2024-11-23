bacf91c6c1c72fa85a0356ac7f6f71c1305a9ee9
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-23 08:50:57 +0000

    red - calculate normal products

--- Cameron.Katka.ClassLibrary/Models/SpecialProduct.cs
+++ Cameron.Katka.ClassLibrary/Models/SpecialProduct.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Cameron.Katka.ClassLibrary.Models
 {
     public class SpecialProduct: Product
---
+++ Cameron.Katka.UnitTests/CalculationTests.cs
@@ -0,0 +1,31 @@
+﻿
+using Cameron.Katka.ClassLibrary.Interfaces;
+using Cameron.Katka.ClassLibrary.Models;
+using Cameron.Katka.ClassLibrary.Repositories;
+using Cameron.Katka.ClassLibrary.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cameron.Katka.UnitTests
+{
+    internal class CalculationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        [Test]
+        public void Calculate_Normal_Products()
+        {
+            _checkoutService.Scan("A");
+            _checkoutService.Scan("B");
+
+            List<Product> products = _basketRepository.GetAllStandardProducts();
+
+            int cost = _calculationService.CalculateNormalProducts(products);
+
+            Assert.IsInstanceOf<int>(totalPrice, "The total price should be an integer.");
+        }
+    }
+}
