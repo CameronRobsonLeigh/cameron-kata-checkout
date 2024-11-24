3e597386a993e63b6a4d64f6984a2705cf8d938a
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-11-24 11:02:40 +0000

    Update Product.cs

--- Cameron.Katka.ClassLibrary/Models/Product.cs
+++ Cameron.Katka.ClassLibrary/Models/Product.cs
@@ -3,7 +3,7 @@ namespace Cameron.Katka.ClassLibrary.Models
 {
     public class Product
     {
-        public string SKU { get; set; }
+        public string SKU { get; }
         public int UnitPrice { get; set; }
 
         public Product(string sku, int unitPrice)
